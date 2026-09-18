d3f518d2b493bafadfe68e568019d7c7b077f39c
Dasun Pubudumal <pubudumald@gmail.com>
2026-09-18 10:30:21 +0100

    chore: initial commit

---
+++ src/weather_llm/__init__.py
@@ -0,0 +1,60 @@
+from typing import Any
+
+from ollama import chat
+
+
+def get_temperature(city: str) -> str:
+    """Get the current temperature for a city
+
+    Args:
+      city: The name of the city
+
+    Returns:
+      The current temperature for the city
+    """
+    temperatures = {
+        "New York": "22°C",
+        "London": "15°C",
+        "Tokyo": "18°C",
+    }
+    return temperatures.get(city, "Unknown")
+
+
+def main() -> None:
+
+    while True:
+        print("To exit, please press enter.")
+        user_input: str = input("Enter a query: ")
+
+        if not user_input or len(user_input) == 0:
+            print("Exiting..")
+            break
+
+        messages: Any = [{"role": "user", "content": user_input}]
+
+        print("Thinking..")
+
+        # pass functions directly as tools in the tools list or as a JSON schema
+        response = chat(
+            model="qwen3", messages=messages, tools=[get_temperature], think=True
+        )
+
+        messages.append(response.message)
+        if response.message.tool_calls:
+            # only recommended for models which only return a single tool call
+            call = response.message.tool_calls[0]
+            result = get_temperature(**call.function.arguments)
+            print("Running the toolchain..")
+            # add the tool result to the messages
+            messages.append(
+                {
+                    "role": "tool",
+                    "tool_name": call.function.name,
+                    "content": str(result),
+                }
+            )
+
+            final_response = chat(
+                model="qwen3", messages=messages, tools=[get_temperature], think=True
+            )
+            print(final_response.message.content)
